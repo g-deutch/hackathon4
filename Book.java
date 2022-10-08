1fe07df7aa50976c3e4e8ea4d1d51b147420f51c
Gabe Deutch <gabedeutch@gmail.com>
2022-10-08 19:45:04 -0400

    Basic full thing

--- Book.java
+++ Book.java
@@ -1,46 +1,52 @@
 import java.util.LinkedList;
 
+
 public class Book {
     String title;
+    String href;
+    String img;
     String genre;
     int copiesAvailable;
-    boolean isAvailable;
     LinkedList<String> locations;
 
     public Book(String title, String genre, LinkedList<String> locations) {
         this.title = title;
+        this.href = title.replaceAll(" ", "");
+        this.img = title.replaceAll(" ", "_");
         this.genre = genre;
         this.locations = locations;
         this.copiesAvailable = locations.size();
-        
     }
-    boolean isAvailable(){
-        return isAvailable;
+
+    boolean isAvailable() {
+        return copiesAvailable > 0;
     }
-    int copiesAvailable(){
+
+    int copiesAvailable() {
         return copiesAvailable;
     }
-    String getTitle(){
+
+    String getTitle() {
         return title;
     }
-    String getGenre(){
+
+    String getGenre() {
         return genre;
     }
+
     boolean checkout(String location){
         for (int x = 0; x < locations.size(); x++){
             if (location.equals(locations.get(x))){
                 copiesAvailable--;
-                if(copiesAvailable <= 0){
-                    isAvailable = false;
-                }
                 return true;
             }
         }
         return false;
         
     }
-    void checkin(){
+    void checkin(String location){
         copiesAvailable++;
+        locations.add(location);
     }
 
 }
---
+++ Main.java
@@ -0,0 +1,66 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        LinkedList<Book> books = new LinkedList<Book>();
+        // columbus, cincinnati, cleavland
+        books.add(new Book("A Tale of Two Cities", "Historical Fiction", new LinkedList<String>(Arrays.asList("Cincinnati", "Cincinnati", "Cincinnati", "Columbus", "Cleveland"))));
+        books.add(new Book("The Hobbit", "Fantasy", new LinkedList<String>(Arrays.asList())));
+        books.add(new Book("The Da Vinci Code", "Mystery Thriller", new LinkedList<String>(Arrays.asList("Columbus", "Columbus", "Columbus", "Colombus"))));
+        books.add(new Book("The Catcher in the Rye", "Coming-of-age", new LinkedList<String>(Arrays.asList("Cleveland", "Cleveland", "Cleveland", "Cleveland", "Cleveland"))));
+        books.add(new Book("Anne of Green Gables", "Children's Novel", new LinkedList<String>(Arrays.asList( "Columbus", "Cleveland", "Cleveland"))));
+        books.add(new Book("Charlotte's Web", "Children's Novel", new LinkedList<String>(Arrays.asList("Cincinnati", "Cincinnati"))));
+        books.add(new Book("The Very Hungry Caterpillar", "Children's Novel", new LinkedList<String>(Arrays.asList("Cincinnati", "Columbus"))));
+        books.add(new Book("To Kill a Mockingbird", "Historical Fiction", new LinkedList<String>(Arrays.asList("Columbus", "Columbus", "Cincinnati", "Cleveland", "Cincinnati"))));
+        makeIndexPage(books);
+    }
+
+    public static void makeIndexPage(LinkedList<Book> books) throws FileNotFoundException {
+        File test = new File("pages/index.html");
+        PrintWriter out = new PrintWriter(test);
+            for(int i = 0; i < books.size(); i++) {
+                printBook(books.get(i), out);
+                bookPages(books.get(i));
+            }
+        out.close();
+    }
+
+    public static void checkoutBook(Book book){
+        
+    }
+
+    public static void bookPages(Book book) throws FileNotFoundException {
+        PrintWriter out = new PrintWriter(new File("pages/" + book.href + ".html"));
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<link rel=\"stylesheet\" href=\"style.css\">");
+        out.println("</head>");
+        out.println("<h1>" + book.title + "</h1>");
+        out.println("<img id = \"book_images\"src=\"../Images/" + book.img + ".jpg\"></img>");
+
+        if(!book.isAvailable()) {
+            out.println("<p id=\"notAvailable\"> not available </p>");
+        } else {
+            out.println("<p id=\"isAvailable\"> available </p>");
+            out.println("<button onclick> Checkout </button>");
+        }
+
+        out.println("<ul>");
+        for(int i = 0; i < book.locations.size(); i++) {
+            out.println("<li>" + book.locations.get(i) + "</li>");
+        }
+        out.println("</ul>");
+        out.println("</html>");
+        out.close();
+    }
+
+    public static void printBook(Book book, PrintWriter out) {
+        out.println("<h1><a href=\"" + book.href + ".html\">" + book.title + "</a></h1>");
+    }
+
+}
